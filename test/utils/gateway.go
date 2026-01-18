c20e06533f6649d3fed3c59d0f0abb868df3286c
Hyeonki Hong <hyeonki.hong@moreh.io>
2026-01-19 01:25:11 +0900

    NO-ISSUE: test(e2e): streamline gateway setup

--- test/utils/gateway.go
+++ test/utils/gateway.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"fmt"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
@@ -40,66 +41,61 @@ func UninstallGatewayInferenceExtension() error {
 	return err
 }
 
-// InstallIstioBase installs Istio base.
-func InstallIstioBase() error {
-	cmd := exec.Command("helm", "repo", "add", "istio", "https://istio-release.storage.googleapis.com/charts")
-	if _, err := Run(cmd); err != nil && !strings.Contains(err.Error(), "already exists") {
-		return fmt.Errorf("failed to add istio helm repo: %w", err)
+// InstallGatewayController installs the gateway controller for the given gateway class.
+func InstallGatewayController(gatewayClass string) error {
+	projectDir, err := GetProjectDir()
+	if err != nil {
+		return fmt.Errorf("failed to get project directory: %w", err)
 	}
 
-	cmd = exec.Command("helm", "repo", "update", "istio")
-	if _, err := Run(cmd); err != nil {
-		return fmt.Errorf("failed to update istio helm repo: %w", err)
-	}
+	switch gatewayClass {
+	case "istio":
+		cmd := exec.Command("helm", "repo", "add", "istio", "https://istio-release.storage.googleapis.com/charts")
+		if _, err := Run(cmd); err != nil && !strings.Contains(err.Error(), "already exists") {
+			return fmt.Errorf("failed to add istio helm repo: %w", err)
+		}
 
-	cmd = exec.Command("helm", "upgrade", "-i", "istio-base", "istio/base",
-		"--version", "1.28.1",
-		"-n", "istio-system",
-		"--create-namespace")
-	_, err := Run(cmd)
-	return err
-}
+		cmd = exec.Command("helm", "repo", "update", "istio")
+		if _, err := Run(cmd); err != nil {
+			return fmt.Errorf("failed to update istio helm repo: %w", err)
+		}
 
-// InstallIstiod installs Istiod.
-func InstallIstiod(valuesPath string) error {
-	helmArgs := []string{
-		"upgrade", "-i", "istiod", "istio/istiod",
-		"--version", "1.28.1",
-		"-n", "istio-system",
-	}
-	if valuesPath != "" {
-		helmArgs = append(helmArgs, "-f", valuesPath)
-	}
-	cmd := exec.Command("helm", helmArgs...)
-	_, err := Run(cmd)
-	return err
-}
+		cmd = exec.Command("helm", "upgrade", "-i", "istio-base", "istio/base",
+			"--version", "1.28.1",
+			"-n", "istio-system",
+			"--create-namespace")
+		if _, err := Run(cmd); err != nil {
+			return err
+		}
 
-// InstallKgatewayCRDs installs KGateway CRDs.
-func InstallKgatewayCRDs() error {
-	cmd := exec.Command("helm", "upgrade", "-i", "kgateway-crds",
-		"oci://cr.kgateway.dev/kgateway-dev/charts/kgateway-crds",
-		"--version", "v2.1.1",
-		"-n", "kgateway-system",
-		"--create-namespace")
-	_, err := Run(cmd)
-	return err
-}
+		cmd = exec.Command("helm", "upgrade", "-i", "istiod", "istio/istiod",
+			"--version", "1.28.1",
+			"-n", "istio-system",
+			"-f", filepath.Join(projectDir, "test/e2e/scripts/base/istiod-values.yaml"))
+		_, err := Run(cmd)
+		return err
 
-// InstallKgateway installs KGateway.
-func InstallKgateway(valuesPath string) error {
-	helmArgs := []string{
-		"upgrade", "-i", "kgateway",
-		"oci://cr.kgateway.dev/kgateway-dev/charts/kgateway",
-		"--version", "v2.1.1",
-		"-n", "kgateway-system",
+	case "kgateway":
+		cmd := exec.Command("helm", "upgrade", "-i", "kgateway-crds",
+			"oci://cr.kgateway.dev/kgateway-dev/charts/kgateway-crds",
+			"--version", "v2.1.1",
+			"-n", "kgateway-system",
+			"--create-namespace")
+		if _, err := Run(cmd); err != nil {
+			return err
+		}
+
+		cmd = exec.Command("helm", "upgrade", "-i", "kgateway",
+			"oci://cr.kgateway.dev/kgateway-dev/charts/kgateway",
+			"--version", "v2.1.1",
+			"-n", "kgateway-system",
+			"-f", filepath.Join(projectDir, "test/e2e/scripts/base/kgateway-values.yaml"))
+		_, err = Run(cmd)
+		return err
+
+	default:
+		return fmt.Errorf("unsupported gateway class: %s", gatewayClass)
 	}
-	if valuesPath != "" {
-		helmArgs = append(helmArgs, "-f", valuesPath)
-	}
-	cmd := exec.Command("helm", helmArgs...)
-	_, err := Run(cmd)
-	return err
 }
 
 // UninstallGatewayController uninstalls the gateway controller for the given gateway class.
